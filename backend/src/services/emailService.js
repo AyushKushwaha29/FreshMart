84c13879534f5f28041b048509e461641aeedef8
Ayush Kushwaha <ayush.kushwaha.29.10.2004@gmail.com>
2026-07-10 12:53:35 +0530

    fix: email otp-service

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -1,11 +1,13 @@
 import nodemailer from "nodemailer";
+import dns from "dns";
+
+
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
-  family: 4,
-  name: "FreshMart",
 
   auth: {
     user: process.env.EMAIL_USER,
